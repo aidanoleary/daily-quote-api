1e1dd39bd6ab908f8117a48255ab9eb0489119ba
Aidan <aidanoleary92@gmail.com>
2017-02-17 12:21:31 +0000

    Updated daily update script

--- daily-update.js
+++ daily-update.js
@@ -1,8 +1,12 @@
 // This is a node script that will be run once daily to update the
 // current daily quote
+var mongoose = require("mongoose");
+
+var serverPort = process.env.PORT || 3000;
+var dbString = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+mongoose.connect(dbString);
 
 var Quote = require("./models/quote.js");
-var mongoose = require("mongoose");
 
 // Update the current daily quote to false
 Quote.findOneAndUpdate({isDaily: true}, {isDaily: false}, function(err, quote) {
